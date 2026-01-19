fe7c465ebde0ee8ad25a083d2ebd7fa0359c8a91
Pablo Mendez <pablo@dappnode.io>
2026-01-19 16:54:14 +0100

    fix get servicename

--- internal/application/domain/stakers.go
+++ internal/application/domain/stakers.go
@@ -116,7 +116,7 @@ func StakerConfigForNetwork(pkg Pkg) StakerConfig {
 		SignerContainerName:      containerName("web3signer", web3signer),
 		BeaconchainContainerName: containerName("beacon-chain", ccDnpName),
 		ValidatorContainerName:   containerName("validator", ccDnpName),
-		ExecutionContainerName:   containerName(serviceNameFromExecutionClient(ecDnpName), ecDnpName),
+		ExecutionContainerName:   containerName(serviceNameFromExecutionClient(ecDnpName, network), ecDnpName),
 		ExecutionVolumeName:      composeVolumeName(pkg.DnpName, pkg.ComposeVolumeName),
 		ExecutionClientShortName: ecShortDnpName,
 	}
@@ -150,12 +150,30 @@ func matchOrRandom(dnpName string, candidates []string) string {
 }
 
 // Utility to get the servicename from a execution client (this utility assumes the service name is the first part of the dnp name)
-func serviceNameFromExecutionClient(dnpName string) string {
-	parts := strings.Split(dnpName, "-")
-	if len(parts) > 0 {
+// e.g hoodi-nethermind.dnp.dappnode.eth -> nethermind
+// e.g nethermind-hoodi.dnp.dappnode.eth -> nethermind
+// e.g nethermind-hoodi.public.dappnode.eth -> nethermind
+// e.g geth.dnp.dappnode.eth -> geth
+// e.g reth-gnosis.dnp.dappnode.eth -> reth
+// e.g gnosis-reth.dnp.dappnode.eth -> reth
+func serviceNameFromExecutionClient(dnpName, network string) string {
+	trimmed := strings.TrimSuffix(dnpName, ".dnp.dappnode.eth")
+	trimmed = strings.TrimSuffix(trimmed, ".public.dappnode.eth")
+	parts := strings.Split(trimmed, "-")
+
+	// If there is only one part, return it
+	if len(parts) == 1 {
 		return parts[0]
 	}
-	return dnpName
+	// If there are multiple parts, find and remove the network part
+	for i, part := range parts {
+		if part == network {
+			parts = append(parts[:i], parts[i+1:]...)
+			break
+		}
+	}
+	// Return the remaining parts joined by "-"
+	return strings.Join(parts, "-")
 }
 
 // Utility to get the container name from service and dnpName. append dnp or public suffix depending on original dnpName
